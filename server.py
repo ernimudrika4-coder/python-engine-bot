b80aa4784450c6a447e98be9d560084613bf574a
ernimudrika4-coder <ernimudrika4@gmail.com>
2026-05-21 23:45:19 +0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {"status": "ok"}
+
+@app.route("/signal", methods=["POST"])
+def signal():
+    data = request.get_json()
+    symbol = data.get("symbol")
+
+    return {
+        "symbol": symbol,
+        "signal": "BUY",
+        "entry": 100,
+        "sl": 90,
+        "tp": 120
+    }
+
+if name == "__main__":
+    app.run(host="0.0.0.0", port=5000)
