b0695f3a93ae74588f1f787ed7e199db94089458
ernimudrika4-coder <ernimudrika4@gmail.com>
2026-05-22 00:24:02 +0700

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "status": "ok",
+        "status": "online",
         "message": "Python engine running"
     })
 
@@ -14,19 +14,17 @@ def home():
 def signal():
     data = request.get_json()
 
-    symbol = data.get("symbol", "BTCUSDT")
+    symbol = data.get("symbol", "XAUUSD")
 
-    response = {
+    return jsonify({
         "symbol": symbol,
         "signal": "BUY",
         "entry": 100,
         "sl": 90,
         "tp": 120
-    }
+    })
 
-    return jsonify(response)
-
-if name == "__main__":
+if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 5000))
